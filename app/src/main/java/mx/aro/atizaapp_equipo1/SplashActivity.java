ee7a73572bc6a25e821ef86a53ce0895896ea910
Arturo Rosas <149410137+A01801099@users.noreply.github.com>
2025-09-29 18:22:07 -0600

    Animacion de Inicio

---
+++ app/src/main/java/mx/aro/atizaapp_equipo1/SplashActivity.java
@@ -0,0 +1,29 @@
+package mx.aro.atizaapp_equipo1;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import mx.aro.atizaapp_equipo1.view.MainActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int DURACION_SPLASH = 3000; // 3 segundos
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, DURACION_SPLASH);
+
+    }
+}
